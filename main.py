3cb03bdf1903dad5650b96c8acc006e56b0b93a1
MarinN <nicolasmarin10123@gmail.com>
2025-06-01 19:27:44 -0400

    gets the link to articles with tickers

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from web_scrapper import webscrapper
 
+PATH_TO_MAIN_PAGE_SOURCE = 'tmp/main_page_source.html'
+
 scrapper = webscrapper()
-scrapper.get_main_page_source()
+
+scrapper.get_main_page_source(PATH_TO_MAIN_PAGE_SOURCE)
+scrapper.get_links_article(PATH_TO_MAIN_PAGE_SOURCE)
--- web_scrapper.py
+++ web_scrapper.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from bs4 import BeautifulSoup
 import time
 
 class webscrapper:
 
+    _WAIT_TIME = 2
+
     _YAHOO_MAIN_PAGE = "https://finance.yahoo.com/news/"
 
     # Collect the main page of yahoo finance news HTML
-    def get_main_page_source(self):
+    def get_main_page_source(self, path_source_file):
         """ options = webdriver.ChromeOptions()
         options.add_argument("--ignore-certificate-error")
         options.add_argument("--ignore-ssl-errors") """
@@ -30,7 +33,7 @@ class webscrapper:
         lastScroll = -1
         currentScroll = 0
         while(lastScroll != currentScroll):
-            time.sleep(1)
+            time.sleep(self._WAIT_TIME)
             lastScroll = currentScroll
             driver.execute_script("window.scrollBy(0, 1000)")
             currentScroll = driver.execute_script("return document.body.scrollHeight")
@@ -38,9 +41,22 @@ class webscrapper:
 
         #save the html source
         source = driver.page_source
-        file = open('tmp/main_page_source.html', 'w+', encoding='utf-8')
+        file = open(path_source_file, 'w+', encoding='utf-8')
         file.write(source)
         file.close()
 
         driver.close()
 
+    def get_links_article(self, path_source_file):
+        file = open(path_source_file, encoding='utf-8')
+        source = file.read()
+        soup = BeautifulSoup(source, "html.parser")
+        
+        #finds all articles that contain a ticker
+        parents = set()
+        tickers = soup.find_all("a", class_='ticker')
+        for t in tickers: 
+            parent = t.find_parent('div', class_='content')
+            parents.add(parent.find('a')["href"])
+        print(parents)
+        
