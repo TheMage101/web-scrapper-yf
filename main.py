053fb61675777d0b8a4770da4093cb2460ff7cd8
MarinN <nicolasmarin10123@gmail.com>
2025-06-01 21:19:32 -0400

    data extraction
    + gets paragrraph from article
    + gets the tickers from article

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from web_scrapper import webscrapper
 
 PATH_TO_MAIN_PAGE_SOURCE = 'tmp/main_page_source.html'
+PATH_TO_ARTICLE_SOURCE = 'tmp/article_source.html'
 
 scrapper = webscrapper()
 
-scrapper.get_main_page_source(PATH_TO_MAIN_PAGE_SOURCE)
-scrapper.get_links_article(PATH_TO_MAIN_PAGE_SOURCE)
+#scrapper.get_main_page_source(PATH_TO_MAIN_PAGE_SOURCE)
+links = scrapper.get_links_article(PATH_TO_MAIN_PAGE_SOURCE)
+data = []
+for l in links:
+    d = {'link': l}
+    scrapper.get_article_source(l)
+    d['article'] = scrapper.get_article(PATH_TO_ARTICLE_SOURCE)
+    d['tickers'] = scrapper.get_tickers(PATH_TO_ARTICLE_SOURCE)
+    print(d['tickers'])
+
+scrapper.get_article(links.pop())
--- web_scrapper.py
+++ web_scrapper.py
@@ -6,37 +6,20 @@ import time
 
 class webscrapper:
 
-    _WAIT_TIME = 2
+
 
     _YAHOO_MAIN_PAGE = "https://finance.yahoo.com/news/"
-
     # Collect the main page of yahoo finance news HTML
     def get_main_page_source(self, path_source_file):
-        """ options = webdriver.ChromeOptions()
-        options.add_argument("--ignore-certificate-error")
-        options.add_argument("--ignore-ssl-errors") """
         driver = webdriver.Firefox()
 
         driver.get(self._YAHOO_MAIN_PAGE)
 
-        #find the accept cookie and clicks
-        try:
-            cookie_button = driver.find_element(by=By.NAME, value="agree")
-            cookie_button.click()
-        except NoSuchElementException:
-            print("Cookie not found")
-        except ElementNotInteractableException:
-            print("No button")
+        self._click_cookie(driver)
 
 
         #load in the entire page by scrolling
-        lastScroll = -1
-        currentScroll = 0
-        while(lastScroll != currentScroll):
-            time.sleep(self._WAIT_TIME)
-            lastScroll = currentScroll
-            driver.execute_script("window.scrollBy(0, 1000)")
-            currentScroll = driver.execute_script("return document.body.scrollHeight")
+        self._load_entire_page(driver)
 
 
         #save the html source
@@ -53,10 +36,71 @@ class webscrapper:
         soup = BeautifulSoup(source, "html.parser")
         
         #finds all articles that contain a ticker
-        parents = set()
+        #makes sure there is no duplicate
+        links = set()
         tickers = soup.find_all("a", class_='ticker')
         for t in tickers: 
             parent = t.find_parent('div', class_='content')
-            parents.add(parent.find('a')["href"])
-        print(parents)
+            links.add(parent.find('a')["href"])
+        return links
+
+    #gets the html source of an article  
+    def get_article_source(self, url: str):
+        driver = webdriver.Firefox()
+        driver.get(url)
         
+        self._click_cookie(driver)
+        self._load_entire_page(driver)
+
+        source = driver.page_source
+        file = open('tmp/article_source.html', 'w+', encoding='utf-8')
+        file.write(source)
+        file.close()
+
+        driver.close()
+
+    _ARTICLE_PARENT_CLASS = 'yf-1ir6o1g' 
+    #returns the content of the article
+    def get_article(self, path):
+        f = open(path, encoding='utf-8')
+        soup = BeautifulSoup(f.read(), 'html.parser')
+
+        parent = soup.find('div', class_=self._ARTICLE_PARENT_CLASS)
+        paragraph = parent.find_all('p')
+        article = ""
+        for p in paragraph:
+            article = article + p.get_text()
+        return article
+
+    _TICKER_PARENT_CLASS = 'yf-pqeumq'
+    def get_tickers(self, path):
+        f = open(path, encoding='utf-8')
+        soup = BeautifulSoup(f.read(), 'html.parser')
+
+        parent = soup.find('div', class_=self._TICKER_PARENT_CLASS)
+        ticker_containers = parent.find_all('a', class_='ticker')
+        tickers = []
+        for t in ticker_containers:
+            tickers.append(t['title'])
+        return tickers
+
+    #--------PRIVATE METHODS--------#
+    
+    _WAIT_TIME = 2
+    def _load_entire_page(self, driver):
+        lastScroll = -1
+        currentScroll = 0
+        while(lastScroll != currentScroll):
+            lastScroll = currentScroll
+            driver.execute_script("window.scrollBy(0, 10000)")
+            time.sleep(self._WAIT_TIME)
+            currentScroll = driver.execute_script("return document.body.scrollHeight")
+
+    def _click_cookie(self, driver):
+        try:
+            cookie_button = driver.find_element(by=By.NAME, value="agree")
+            cookie_button.click()
+        except NoSuchElementException:
+            print("Cookie not found")
+        except ElementNotInteractableException:
+            print("No button")
